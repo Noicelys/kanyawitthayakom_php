4b56f7615ef0657ad7566f95399f5ff167cdac87
Noicely <161154392+Noicelys@users.noreply.github.com>
2025-04-18 14:32:26 +0700

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -3,6 +3,6 @@ session_start();
 session_unset();
 session_destroy();
 
-echo "<script>alert('คุณได้ออกจากระบบเรียบร้อยแล้ว'); window.location.href = 'Login.php';</script>";
+echo "<script>alert('คุณได้ออกจากระบบเรียบร้อยแล้ว'); window.location.href = 'index.php';</script>";
 exit();
 ?>
