af9b25e9ae2de3a4f8f3bc22a03616f41f4ef4d3
Noicely <161154392+Noicelys@users.noreply.github.com>
2025-04-18 14:32:52 +0700

    Update check_student.php

--- check_student.php
+++ check_student.php
@@ -66,6 +66,7 @@ if (isset($_POST['submit_attendance'])) {
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="rms_main_M.css">
     <style>
         body {
             font-family: 'Noto Sans Thai', sans-serif;
